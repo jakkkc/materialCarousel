44a70a95e70b8991b8d4f737f318ebbec69527b3
dexter <jacmwaniki@gmail.com>
2023-03-27 10:47:14 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import { AutoPlay } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    background: "#fff",
+  },
+  header: {
+    fontSize: "2rem",
+    fontWeight: "bold",
+    marginBottom: "2rem",
+    textAlign: "center",
+  },
+  carouselContainer: {
+    width: "50%",
+    margin: "auto",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+  },
+  carouselItem: {
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Typography className={classes.header}>This is the About Page</Typography>
+      <div className={classes.carouselContainer}>
+        <AutoPlay>
+          <div className={classes.carouselItem}>
+            <Typography variant="h3">1</Typography>
+          </div>
+          <div className={classes.carouselItem}>
+            <Typography variant="h3">2</Typography>
+          </div>
+          <div className={classes.carouselItem}>
+            <Typography variant="h3">3</Typography>
+          </div>
+          <div className={classes.carouselItem}>
+            <Typography variant="h3">4</Typography>
+          </div>
+        </AutoPlay>
+      </div>
     </div>
   );
 }
